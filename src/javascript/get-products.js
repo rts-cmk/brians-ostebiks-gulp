a94257275882dad3759b95fe746704a2c14373b7
Brian Emilius <brian@brianemilius.com>
2019-10-24 15:08:25 +0200

    add even more filessssssss

--- src/javascript/get-products.js
+++ src/javascript/get-products.js
@@ -2,6 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
 	fetch("/data/produkter.json")
 		.then(response => response.json())
 		.then(function(data) {
-			console.log(data);
-		})
+			const cardTemplate = document.getElementById("cardTemplate");
+			const list = document.getElementsByClassName("cardList")[0];
+
+			data.forEach(function(product) {
+				const clone = cardTemplate.content.cloneNode(true);
+				clone.querySelector("h1").innerText = product.navn;
+				clone.querySelector("p").innerText = product.beskrivelse[0];
+				clone.querySelector("img").src = product.billeder[0];
+				clone.querySelector(".price").innerText = product.pris;
+				clone.querySelector(".weight").innerText = product.vægt;
+				clone.querySelector(".country").innerText = product.land;
+				clone.querySelector("a").href = `/ost/?sku=${product.sku}`;
+				list.appendChild(clone);
+			});
+		});
 });
